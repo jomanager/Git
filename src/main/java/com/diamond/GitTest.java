5f576d2e5e707eecdc5d7655b1f0a2e18d55ca61
skw <skw@diamond.com>
2021-12-06 15:42:59 +0800

    push commit

--- src/main/java/com/diamond/GitTest.java
+++ src/main/java/com/diamond/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello master!");
         System.out.println("hello got-fix!");
+        System.out.println("hello pushTest!");
     }
-
 }
